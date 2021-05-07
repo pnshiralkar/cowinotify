e9101d4d7e059109e1fc1a436046afdf5df5f03f
pnshiralkar <pnshiralkar@gmail.com>
2021-05-07 20:11:57 +0530

    Send SMS on error, use Host and User Agent headers to bypass cloudflare security

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import datetime
 import time
+from simplejson.errors import JSONDecodeError
 
 import pytz as pytz
 import requests
 
-from config import PINCODES_PHONE_NUMBERS, RESEND_SMS_INTERVAL, CHECK_INTERVAL, MOCK_SMS
+from config import PINCODES_PHONE_NUMBERS, RESEND_SMS_INTERVAL, CHECK_INTERVAL, MOCK_SMS, ERROR_SMS_INTERVAL, \
+    ERROR_SMS_NUMBERS
 from sms import send_sms
 
 cache = {}
@@ -24,12 +26,18 @@ def get_time_now():
     return datetime.datetime.now(tz=tz)
 
 
+last_error_sms_time = get_time_now()
+
+
 def check_availability(pincode, date):
     """
     available, message = check_availability('415110', '03-05-2021')
     """
     resp = requests.get(
-        f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={pincode}&date={date}"
+        f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={pincode}&date={date}",
+        headers={"Host": "cdn-api.co-vin.in",
+                 "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
+                 }
     )
     # print(resp.json())
     available_in_pincode = False
@@ -57,27 +65,44 @@ def check_availability(pincode, date):
 
 
 def check():
+    global last_error_sms_time
     num_sms_sent = 0
     date = get_time_now().strftime("%d-%m-%Y")
-    for pc in PINCODES_PHONE_NUMBERS:
-        available, message = check_availability(pc, date)
-        if available:
-            should_send = False
-            if pc not in cache:
-                should_send = True
-            else:
-                delta = get_time_now() - cache[pc]['time']
-                if cache[pc]['message'] != message or delta.seconds >= RESEND_SMS_INTERVAL:
+    try:
+        for pc in PINCODES_PHONE_NUMBERS:
+            available, message = check_availability(pc, date)
+            if available:
+                should_send = False
+                if pc not in cache:
                     should_send = True
+                else:
+                    delta = get_time_now() - cache[pc]['time']
+                    if cache[pc]['message'] != message or delta.seconds >= RESEND_SMS_INTERVAL:
+                        should_send = True
 
-            if should_send:
-                cache[pc] = {
-                    'message': message,
-                    'time': get_time_now()
-                }
-                for phone in PINCODES_PHONE_NUMBERS[pc]:
-                    num_sms_sent += 1
-                    send_sms(phone, message, mock=MOCK_SMS)
+                if should_send:
+                    cache[pc] = {
+                        'message': message,
+                        'time': get_time_now()
+                    }
+                    for phone in PINCODES_PHONE_NUMBERS[pc]:
+                        num_sms_sent += 1
+                        send_sms(phone, message, mock=MOCK_SMS)
+    except JSONDecodeError:
+        print("JSON Decode Error")
+        delta = get_time_now() - last_error_sms_time
+        if delta.seconds >= ERROR_SMS_INTERVAL:
+            for phone in ERROR_SMS_NUMBERS:
+                send_sms(phone, "JSON Decode error occurred in CowiNotify", mock=MOCK_SMS)
+            last_error_sms_time = get_time_now()
+    except:
+        print("Some Error")
+        delta = get_time_now() - last_error_sms_time
+        if delta.seconds >= ERROR_SMS_INTERVAL:
+            for phone in ERROR_SMS_NUMBERS:
+                send_sms(phone, "Some error occurred in CowiNotify", mock=MOCK_SMS)
+            last_error_sms_time = get_time_now()
+
     return num_sms_sent
 
 
